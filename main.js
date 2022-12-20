f53c15662e2addf8f0348f7c1c1d57a6e4a5ec64
D1saster20 <yuriizhukevych@gmail.com>
2022-12-20 20:40:07 +0200

    1

---
+++ main.js
@@ -0,0 +1,68 @@
+// let x = -1
+// if (x){
+//     console.log('вірно')
+// }else{
+//     console.log('невірно')
+// }
+
+let time = 46
+if ( time < 16){
+    console.log('1')
+}else if(15 < time && time < 31){
+    console.log('2')
+}else if(30 < time && time < 46){
+    console.log('3')
+}else if(45 < time && time < 60){
+    console.log('4')
+}
+
+// let day = 19
+// if ( day < 11){
+//     console.log('1')
+// }else if(10 <  day &&  day < 21){
+//     console.log('2')
+// }else if(20 <  day &&  day < 32) {
+//     console.log('3')
+// }else {
+//     console.log('ти за яким календарем живеш?')
+// }
+
+// switch ('1'){
+//     case '1':
+//         console.log('monday');
+//         break;
+//     case '2':
+//         console.log('tuesday');
+//         break;
+//     case '3':
+//         console.log('wednesday');
+//         break;
+//     case '4':
+//         console.log('thursday');
+//         break;
+//     case '5':
+//         console.log('friday');
+//         break;
+//     case '6':
+//         console.log('saturday');
+//         break;
+//     case '7':
+//         console.log('sunday');
+//         break;
+//     default:
+//         console.log('???????');
+// }
+
+// let first = 2
+// let second = 2
+// if (first<second){
+//     console.log(second)
+//
+// }else if (first>second){
+//     console.log(first)
+// }else   {
+//     console.log('the numbers are the same')
+// }
+
+// let x = prompt() || 'default';
+// console.log(x);
